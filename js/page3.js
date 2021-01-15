34acdb36c2ce1ec58dfd7e3726fe884ef68ff72a
tajeshwar <tkhara@gmail.com>
2021-01-15 12:00:45 +0530

    Moved project over to using css grid. Only page 5 is left. Some decisions on fonts need to be made.

--- js/page3.js
+++ js/page3.js
@@ -1,6 +1,6 @@
 // Sound on page 3
 var audio = new Audio("./media/sound2.mp3");
-let soundDiv = document.querySelector('#sound-div')
+let soundDiv = document.querySelector('#item-5')
 
 soundDiv.addEventListener('click', (e) => {
     audio.play()
